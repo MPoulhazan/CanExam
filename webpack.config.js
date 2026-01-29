9a6575b0f2b8d69fc7b5c2a143c34a31b5f84b6c
Mickael POULHAZAN <mp041@hotmail.fr>
2026-01-28 20:28:49 -0500

    Fix optimization object initialization

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,12 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
-    const config = await createExpoWebpackConfigAsync(
-        {
-            ...env,
-            mode: 'development',
-        },
-        argv
-    );
+    const config = await createExpoWebpackConfigAsync(env, argv);
 
     // Disable minification to avoid Terser errors with Flow and script files
+    if (!config.optimization) {
+        config.optimization = {};
+    }
     config.optimization.minimize = false;
 
     return config;
