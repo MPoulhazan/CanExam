6507c383f70553eccb21d8678f0fdca923626c96
Mickael POULHAZAN <mp041@hotmail.fr>
2026-01-28 20:22:37 -0500

    Exclude more problematic files from Terser

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,15 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 module.exports = async function (env, argv) {
     const config = await createExpoWebpackConfigAsync(env, argv);
 
-    // Exclude .flow.js files from Terser minification
+    // Exclude problematic files from Terser minification
     if (config.optimization && config.optimization.minimizer) {
         config.optimization.minimizer.forEach((plugin) => {
             if (plugin.constructor.name === 'TerserPlugin') {
-                plugin.options.exclude = /\.flow\.js$/;
+                plugin.options.exclude = [
+                    /\.flow\.js$/,
+                    /node_modules\/@swc\/helpers\/scripts/,
+                    /node_modules\/styleq/,
+                ];
             }
         });
     }
