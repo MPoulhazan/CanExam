cb6e2b8197457c1085e0fb061cd262013c2b5eb5
Mickael POULHAZAN <mp041@hotmail.fr>
2026-01-28 19:53:35 -0500

    Remove react-native imports from _app.tsx

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,13 +1,9 @@
 import type { AppProps } from 'next/app';
 import React from 'react';
-import { AppRegistry } from 'react-native';
 import '../styles.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
 }
-
-// Register a default app name for react-native-web
-AppRegistry.registerComponent('Main', () => MyApp);
 
 export default MyApp;
