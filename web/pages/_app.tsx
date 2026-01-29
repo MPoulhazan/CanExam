23778b3b31f5c1f2dd6e428420cdc2eaf2284f25
Mickael POULHAZAN <mp041@hotmail.fr>
2026-01-28 20:04:13 -0500

    Switch to Expo web build instead of Next.js

--- web/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import type { AppProps } from 'next/app';
-import React from 'react';
-import '../styles.css';
-
-function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
-}
-
-export default MyApp;
