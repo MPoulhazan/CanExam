ef7f9bf5fb6d4ebe91af31aeffcd939d8e594907
Mickael POULHAZAN <mp041@hotmail.fr>
2026-01-28 19:59:50 -0500

    Simplify next.config.js - remove react-native dependencies

--- web/next.config.js
+++ web/next.config.js
@@ -1,13 +1,4 @@
-const withTM = require('next-transpile-modules')(['react-native']);
-
-module.exports = withTM({
+module.exports = {
     reactStrictMode: true,
     turbopack: {},
-    webpack: (config) => {
-        config.resolve.alias = {
-            ...(config.resolve.alias || {}),
-            'react-native$': 'react-native-web',
-        };
-        return config;
-    },
-});
+};
